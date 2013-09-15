beaac4f45873753109c5680cf0a26a83f4586386
rodrigo <r.sanabriadc@gmail.com>
2013-09-15 11:16:18 -0500

    5

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-mn = []
+mne = []
 
 
 def mn():
 	global mn
 	f = open("nmemonicos.txt",'r')
 	for i in f.readlines():
-		a = i.split("\n")
-		mn.append(a[0])
+		a = i.split(",")
+		a[7] = a[7].split("\n")[0]
+		mne.append(a)
 
 
 
 
-		
+	
 mn()
-		
+print len(mne[49][6])
+
 
 		
 
