71cd2bf813fd4c461d14f147257d2ecba2bd1ec9
Rodrigo <r.sanabriadc@gmail.com>
2013-10-13 10:31:37 -0500

    version casi final

--- main.py
+++ main.py
@@ -1,24 +1,134 @@
+import re
+#Variables Y Constante
+archivo = []
+archivo_lineas = []
 mne = []
-
-
+mnemonicos =[]
+mne_inh = []
+mne_rel = []
+reservadas = ["EQU", "ORG", "FCB", "END"]
+dvariables = {}
+valor_v = []
 def mn():
-	global mn
+	global mne
 	f = open("mnemonicos.txt",'r')
 	for i in f.readlines():
 		a = i.split(",")
 		a[7] = a[7].split("\n")[0]
 		mne.append(a)
+	for i in mne:
+		mnemonicos.append(i[0])
+	
+	f.close()
 
+def escribirarchivo():
+	global f
+	f = open("compliado.lst", "w")
+	 
+def mne_INH():
+	global mne_inh
+	for i in mne:
+		if i[1] ==""and i[2]=="" and i[3] == "" and i[4]== "" and i[5] == "" and i[7] == "":
+			mne_inh.append(i[0])
 
+def mne_REL():
+	global mne_rel
+	for i in mne:
+		if i[1] ==""and i[2]=="" and i[3] == "" and i[4]== "" and i[5] == "" and i[6] == "":
+			mne_rel.append(i[0])
+			
+def leerarchivo():
+	archivo_l = []
+	f = open("EJEMPLO.ASC", "r")
+	for i in f.readlines():
+		a = i.split(" ")
+		for j in a:
+			#print j
+			if len(j) < 3:
+				pass
+			else:
+				archivo_l.append(j)
+				if "\n" in j:
+					archivo_lineas.append(archivo_l)
+					archivo_l = []
+		archivo.append(i)
+				
+			
+		
+		
+	
+def inh(i):
+	global m
+	if "$" in i[len(i)-1]:
+		m = i[len(i)-1].split("$")[1]
+		if i[0] in mne_inh:
+			return True
+	
+		
+def inmediato(operando):
+	if "$" in operando:
+		operando = operando.split("$")[1]
+	if "#" in operando:
+		pass
+	return operando
 
+def directo(operando):
+	if "$" in operando:
+		operando = operando.split("$")[1]
+	operando = operando[:2]
+	return operando
+	
+def ext(operando):
+	if "$" in operando:
+		operando = operando.split("$")[1]
+	return operando
+	
+def ind(operando):
+	global x, y
+	if "$" in operando:
+		operando = operando.split("$")[1]
+	if "X" in operando:
+		ox = operando.split(",")
+		operando = ox[0]
+		x = ox[1]
+	elif "Y" in operando:
+		oy = operando.split(",")
+		operando = oy[0]
+		y = ox[1]
+	return operando
 
+def rel(i):
+	global m
+	if "$" in i[len(i)-1]:
+		m = i[len(i)-1].split("$")[1]
+		if m in mne_rel:
+			return True
+def variables():
+	global variables
+	for i in archivo:
+		if "EQU" in i:
+			a = i.split(" ")
+			dvariables[a[0]] = a[len(a)-1].split("\r")[0]
 	
 mn()
-print len(mne[49][6])
-
-
-		
-
-
+leerarchivo()
+escribirarchivo()
+variables()
+mne_INH()
+mne_REL()
+	
+def principal():
+	count = 1
+	inst = "    "
+	for i in archivo:
+		if "*" in i:
+			f.write(str(count)+"A "+inst+"   "+str(i))
+		else:
+			f.write(str(count)+"A "+ str(i))
+			
+		count +=1
+	
+#print archivo_lineas[4]
+principal()
 
 		
