1f4af2d8b7a8c0730f42ca4fbfd37501cc4db850
Ubuntu <ubuntu@ip-172-31-27-134.us-west-2.compute.internal>
2013-10-15 04:13:52 +0000

    version 7

--- main.py
+++ main.py
@@ -39,38 +39,43 @@ def mne_REL():
 			mne_rel.append(i[0])
 			
 def leerarchivo():
-	global lista, lista_lineas,f
-	archivo_l = []
-	a =[]
-	b = []
-	lista_lineas = []
-	f = open("EJEMPLO.ASC", "r")
-	for i in f.readlines():
-		lista_lineas.append(i.split(" "))
-		archivo.append(i)
-	for j in lista_lineas:
-			b.append(a)
-			a = []
-			for k in j:
-				if len(k)== 0:
-					pass
-				elif "*" in k and j[0] != "*":
-					break
-				else:
-					if "\r\n" in k:
-						k = k.split("\r\n")[0]
-						a.append(k)
-					elif "\r" in k:
-						print k
-						if len(k)== 1:
-							k = ""
-							a.append(k)
-						else:
-							k = k.split("\r")[0]
-							a.append(k)
-					else:
-						a.append(k)
-	lista_lineas = b
+        global lista, lista_lineas,f
+        archivo_l = []
+        a =[]
+        b = []
+        lista_lineas = []
+        f = open("EJEMPLO.ASC", "r")
+        for i in f.readlines():
+                lista_lineas.append(i.split(" "))
+                archivo.append(i)
+        for j in lista_lineas:
+                        b.append(a)
+                        a = []
+                        for k in j:
+                                if len(k)== 0:
+                                        pass
+                                elif "*" in k and j[0] != "*":
+                                        break
+                                else:
+                                        if "\r\n" in k:
+                                                k = k.split("\r\n")[0]
+                                                a.append(k)
+                                        elif "\r" in k:
+                                                print k
+                                                if len(k)== 1:
+                                                        k = ""
+                                                        a.append(k)
+                                                else:
+                                                        k = k.split("\r")[0]
+                                                        a.append(k)
+                                        else:
+                                                a.append(k)
+        lista_lineas = b
+        count = 0
+        for i in lista_lineas:
+                if len(i) == 0:
+                        lista_lineas[count] = [" "]
+                count += 1
 		
 				
 			
@@ -138,17 +143,46 @@ mne_INH()
 mne_REL()
 	
 def principal():
-	count = 1
-	inst = "    "
-	for i in archivo:
-		if "*" in i:
-			f.write(str(count)+"A "+inst+"   "+str(i))
-		else:
-			f.write(str(count)+"A "+ str(i))
-			
-		count +=1
-	f.close()
+        count = 0
+        inst = "    "
+        memoria = ""
+        contador_memoria = 0
+        a = ""
+        for i in range(0,len(archivo)):
+                if lista_lineas[i][0] == reservadas[1] and count == 0:
+                       a = (int((lista_lineas[i][len(lista_lineas[i])-1].split("$")[1]),16))#+ 0x8000
+                       count = 1
+                else:
+                        if archivo[i][0] == "*":
+                                f.write(str(i)+"A "+inst+"   "+archivo[i])
+                                if a != "":
+                                        a = a+8
+                                
+                        else:
+                                if a != "":
+                                        print a
+                                        a = a+8
+                                        f.write(str(i)+"A "+hex(a).split("x")[1].upper()+" "+ archivo[i])
+                                else:
+                                        f.write(str(i)+"A "+a+ archivo[i])
+                
+
+##                if count-1 == 60:
+##                        print lista_lineas[count-1]
+##                if "*" in i:
+##                        f.write(str(count)+"A "+inst+"   "+str(i))
+##                else:
+##                        if lista_lineas[count-1][0] == reservadas[1]:
+##                                print "entra en linea", count
+##                                contador_memoria = int((lista_lineas[count-1][len(lista_lineas[count-1])-1].strip("$")[0]).encode('hex'),16)
+##                        else:
+##                                contador_memoria = str("")
+##                f.write(str(count)+"A "+str(contador_memoria)+ str(i))
+##                if type(contador_memoria) == int:
+##                        contador_memoria += 1
+##                count +=1
+                #count += 1
+        f.close()
 	
-#print archivo_lineas[4]
 principal()
-print len(archivo),len(lista_lineas)
+
