49f94d73af9c66933c8364bc8970fd55c5175f0c
Rodrigo <r.sanabriadc@gmail.com>
2013-10-16 20:13:44 -0500

    version 10

--- main.py
+++ main.py
@@ -25,7 +25,12 @@ def mn():
 def escribirarchivo():
 	global f
 	f = open("compliado.lst", "w")
-	 
+def mne_EXT():
+	global mne_ext
+	mne_ext = []
+	for i in mne:
+		if i[5] != "":
+			mne_ext.append(i[0])
 def mne_INH():
 	global mne_inh
 	for i in mne:
@@ -81,58 +86,7 @@ def leerarchivo():
                 if len(i) == 0:
                         lista_lineas[count] = [" "]
                 count += 1
-		
-				
-			
-		
-		
-	
-def inh(i):
-	global m
-	if "$" in i[len(i)-1]:
-		m = i[len(i)-1].split("$")[1]
-		if i[0] in mne_inh:
-			return True
-	
-		
-def inmediato(operando):
-	if "$" in operando:
-		operando = operando.split("$")[1]
-	if "#" in operando:
-		pass
-	return operando
 
-def directo(operando):
-	if "$" in operando:
-		operando = operando.split("$")[1]
-	operando = operando[:2]
-	return operando
-	
-def ext(operando):
-	if "$" in operando:
-		operando = operando.split("$")[1]
-	return operando
-	
-def ind(operando):
-	global x, y
-	if "$" in operando:
-		operando = operando.split("$")[1]
-	if "X" in operando:
-		ox = operando.split(",")
-		operando = ox[0]
-		x = ox[1]
-	elif "Y" in operando:
-		oy = operando.split(",")
-		operando = oy[0]
-		y = ox[1]
-	return operando
-
-def rel(i):
-	global m
-	if "$" in i[len(i)-1]:
-		m = i[len(i)-1].split("$")[1]
-		if m in mne_rel:
-			return True
 def variables():
 	global variables
 	for i in archivo:
@@ -140,12 +94,29 @@ def variables():
 			a = i.split(" ")
 			dvariables[a[0]] = a[len(a)-1].split("\r")[0]
 	
+
+def etiquetas():
+	global etiq
+	etiq = { }
+	cont = 0
+	for i in lista_lineas:
+		if i[0] == "ORG":
+			cont = int(i[-1].split("$")[1],16)
+		if len(i) == 1 and len(i[0])>=4:
+			etiq[i[0]] = hex(cont).split("x")[1].upper()
+			
+			
+			
+		cont += 8
+	
 mn()
 leerarchivo()
 escribirarchivo()
 variables()
 mne_INH()
 mne_REL()
+mne_EXT()
+etiquetas()
 	
 def principal():
         count = 0
@@ -153,45 +124,98 @@ def principal():
         memoria = ""
         contador_memoria = 0
         a = ""
+	c = ""
         for i in range(0,len(archivo)):
                 n = ""
                 n_o = ""
                 o = ""
+		c = lista_lineas[i][-1]
+		if "$" in c:
+			c = c.split("$")[1]
+		
+		if c in dvariables.keys():
+                        c = dvariables[c]
+			if "$" in c:
+				c = c.split("$")[1]
                 if lista_lineas[i][0] == reservadas[1] :#nd count == 0:
                        a = (int((lista_lineas[i][len(lista_lineas[i])-1].split("$")[1]),16))#+ 0x8000
                        count = 1
-                else:
-                        if archivo[i][0] == "*" or len(archivo[i].strip(" "))<2 :
-                                f.write(str(i+1)+"A "+inst+"   "+archivo[i])
+                else:				
+                        if archivo[i-1][0] == "*" or len(archivo[i].strip(" "))<2 :
+                                f.write(str(i+1)+"A "+inst+"    "+archivo[i-1])
                                 if a != "":
                                         a = a+8
                                 
                         else:
-                                if lista_lineas[i][0] in mnemonicos:
-                                        n = lista_lineas[i][0]
-                                        o = lista_lineas[i][-1]
-                                        if "#" in lista_lineas[i][-1]:
+				if "EQU" in archivo[i]:
+					o = lista_lineas[i][-1]
+					if "$" in o:
+						o = o.split("$")[1]
+				
+					
+                                elif lista_lineas[i][0] in mnemonicos:                                        
+                                        if "#" in lista_lineas[i][-1] and lista_lineas[i][0] not in mne_inh and lista_lineas[i][0]:
+						n = lista_lineas[i][0]
+						o = lista_lineas[i][-1]
                                                 o = o.split("#")[1]
                                                 if "$" in o:
                                                         o = o.split("$")[1]
                                                 if o in dvariables.keys():
                                                         o = dvariables[o]
+							if "$" in o:
+								o = o.split("$")[1]
+						elif o in etiq.keys():
+							o = etiq[o]
                                                 for t in mne:
                                                         if t[0] == n:
                                                                 n_o = t[1]
+					elif lista_lineas[i][0] in mne_inh:
+						n = lista_lineas[i][0]
+						o = ""
+						if o in etiq.keys():
+							o = etiq[o]
+						for r in mne:
+							if r[0] == n:
+								#print i,r, t[6]
+								n_o = r[6]
+					elif lista_lineas[i][0] in mne_rel:
+						n = lista_lineas[i][0]
+						if o in etiq.keys():
+							o = etiq[o]
+					elif c[0:2] == "00" and not "," in c and lista_lineas[i][0] not in mne_ext:
+						n = lista_lineas[i][0]
+						o = c[2:]
+						for h in mne:
+							if h[0] == n:
+								n_o = h[2]
+								if n_o == "":
+									o = ""
+					elif lista_lineas[i][0] in mne_ext:
+						n = lista_lineas[i][0]
+						o = c
+						if o in etiq.keys():
+							o = etiq[o]
+					
+						for d in mne:
+							if d[0] == n:
+								n_o = d[5]
+							     
+						
+					
                                                                 
                                                                 
                                 if a != "":
                                         a = a+8
                                         if n_o != "":
-                                                f.write(str(i+1)+"A "+hex(a).split("x")[1].upper()+" "+n_o+o + archivo[i-1])
+                                                f.write(str(i+1)+"A "+hex(a).split("x")[1].upper()+" "+n_o+o +"    "+ archivo[i-1])
                                         else:
-                                                f.write(str(i+1)+"A "+hex(a).split("x")[1].upper()+" " + archivo[i-1])
+                                                f.write(str(i+1)+"A "+hex(a).split("x")[1].upper()+" "+o+"    "+ archivo[i-1])
                                 else:
-                                        f.write(str(i+1)+"A "+a+ archivo[i])
+                                        f.write(str(i+1)+"A "+a+" "+o+" "+ "    "+ archivo[i-1])
                 
                 
         f.close()
-
 principal()
 
+
+print etiq
